b255aed1ce2d6634d1e2345010c5dc8ac2bc162b
kavitagashni <107977816+kavitagashni@users.noreply.github.com>
2023-04-03 06:56:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
-import TourData from'./Data.json'
+import TourData from './Data.json'
 import './component/Card'
 import Card from './component/Card';
 import { useState } from 'react';
 
 function App() {
-const [data, setData]= useState(TourData);
+  const [data, setData] = useState(TourData);
 
 
-function ClickOnNotInterested (){
-  setData([]);
- 
-}
+  function ClickOnNotInterested(id) {
+    const checkList = data.filter((element, index) => {
+      return id !== index
+    })
+    setData(checkList);
+  }
+
 
 
   return (
     <div className="App">
       <h1>Our Tours</h1>
-      <hr/>
-      <Card data={data} ClickOnNotInterested={ClickOnNotInterested}/>
+      <hr />
+      <Card data={data} ClickOnNotInterested={ClickOnNotInterested} />
     </div>
   );
 }
