b7695b53cf4b4362abe451c79d5301af8e469674
kavitagashni <107977816+kavitagashni@users.noreply.github.com>
2023-04-03 06:54:07 +0000

    Update Card.js

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,12 +4,19 @@ import { useState } from 'react';
 function Card(props) {
 
     const [showMore, setShowMore] = useState(true);
-    const[text, setText] =useState(true);
-     
-    const ToggleSwitch=()=>{
-        showMore?setShowMore(false):setShowMore(true);
-        text? setText(false): setText(true);
+
+    const ToggleSwitch = (id) => {
+        for (let i = 0; i < props.data.length; i++) {
+            if (id == i) {
+                setShowMore(!showMore);
+            } else{
+                console.log("else")
+                setShowMore(showMore)
+            }
+        }
     }
+
+
 
     return (
         <div className="box">
@@ -23,8 +30,8 @@ function Card(props) {
                         <div className='infoBox'>
                             <h2>{event.name}</h2>
                             <p className={showMore ? "Show_less" : "Show_more"}>{event.info}</p>
-                            <p className="reed_more"  onClick={() => setShowMore(!showMore)}>{showMore? "...Reed More":""}</p>
-                            <button className='notInterested' onClick={props.ClickOnNotInterested}>Not Interested</button>
+                            <p className="reed_more" onClick={() => ToggleSwitch(index)}>{showMore ? "...Reed More" : "...Reed Less"}</p>
+                            <button className='notInterested' onClick={() => props.ClickOnNotInterested(index)}>Not Interested</button>
                         </div>
                     </div>)
             })}
